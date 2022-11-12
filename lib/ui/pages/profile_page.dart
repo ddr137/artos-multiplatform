fde9c3dfd6cc1042c5f318419264198a33e15402
ddr <dadanramdhani1509@gmail.com>
2022-11-12 20:41:55 +0700

    - Implement user information

--- lib/ui/pages/profile_page.dart
+++ lib/ui/pages/profile_page.dart
@@ -35,113 +35,121 @@ class ProfilePage extends StatelessWidget {
               child: CircularProgressIndicator(),
             );
           }
-          return ListView(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 24,
-            ),
-            children: [
-              const SizedBox(height: 30),
-              Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 30,
-                  vertical: 22,
-                ),
-                decoration: BoxDecoration(
-                  color: whiteColor,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Column(
-                  children: [
-                    Container(
-                      width: 120,
-                      height: 120,
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                          image: AssetImage('assets/img_profile.png'),
-                        ),
-                      ),
-                      child: Align(
-                        alignment: Alignment.topRight,
-                        child: Container(
-                          width: 28,
-                          height: 28,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: whiteColor,
+
+          if (state is AuthSuccess) {
+            return ListView(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 24,
+              ),
+              children: [
+                const SizedBox(height: 30),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 30,
+                    vertical: 22,
+                  ),
+                  decoration: BoxDecoration(
+                    color: whiteColor,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    children: [
+                      Container(
+                        width: 120,
+                        height: 120,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            image: state.user.profilePicture == null
+                                ? const AssetImage('assets/img_profile.png')
+                                : NetworkImage(state.user.profilePicture!)
+                                    as ImageProvider,
                           ),
-                          child: Center(
-                            child: Icon(
-                              Icons.check_circle,
-                              color: greenColor,
-                              size: 24,
+                        ),
+                        child: state.user.verified == 1 ? Align(
+                          alignment: Alignment.topRight,
+                          child: Container(
+                            width: 28,
+                            height: 28,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: whiteColor,
+                            ),
+                            child: Center(
+                              child: Icon(
+                                Icons.check_circle,
+                                color: greenColor,
+                                size: 24,
+                              ),
                             ),
                           ),
+                        ) : null,
+                      ),
+                      const SizedBox(height: 16),
+                      Text(
+                        state.user.username!,
+                        style: blackTextStyle.copyWith(
+                          fontSize: 18,
+                          fontWeight: medium,
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 16),
-                    Text(
-                      'Dadan Ramdhani',
-                      style: blackTextStyle.copyWith(
-                        fontSize: 18,
-                        fontWeight: medium,
+                      const SizedBox(height: 40),
+                      ProfileMenuItem(
+                        iconUrl: 'assets/ic_edit_profile.png',
+                        title: 'Edit Profile',
+                        onTap: () async {
+                          if (await Navigator.pushNamed(context, '/pin') ==
+                              true) {
+                            Navigator.pushNamed(context, '/profile-edit');
+                          }
+                        },
                       ),
-                    ),
-                    const SizedBox(height: 40),
-                    ProfileMenuItem(
-                      iconUrl: 'assets/ic_edit_profile.png',
-                      title: 'Edit Profile',
-                      onTap: () async {
-                        if (await Navigator.pushNamed(context, '/pin') ==
-                            true) {
-                          Navigator.pushNamed(context, '/profile-edit');
-                        }
-                      },
-                    ),
-                    ProfileMenuItem(
-                      iconUrl: 'assets/ic_pin.png',
-                      title: 'My PIN',
-                      onTap: () async {
-                        if (await Navigator.pushNamed(context, '/pin') ==
-                            true) {
-                          Navigator.pushNamed(context, '/profile-edit-pin');
-                        }
-                      },
-                    ),
-                    ProfileMenuItem(
-                      iconUrl: 'assets/ic_wallet.png',
-                      title: 'Wallet Settings',
-                      onTap: () {},
-                    ),
-                    ProfileMenuItem(
-                      iconUrl: 'assets/ic_reward.png',
-                      title: 'My Reward',
-                      onTap: () {},
-                    ),
-                    ProfileMenuItem(
-                      iconUrl: 'assets/ic_help.png',
-                      title: 'Help Center',
-                      onTap: () {},
-                    ),
-                    ProfileMenuItem(
-                      iconUrl: 'assets/ic_logout.png',
-                      title: 'Logout',
-                      onTap: () {
-                        context.read<AuthBloc>().add(AuthLogout());
-                      },
-                    ),
-                  ],
+                      ProfileMenuItem(
+                        iconUrl: 'assets/ic_pin.png',
+                        title: 'My PIN',
+                        onTap: () async {
+                          if (await Navigator.pushNamed(context, '/pin') ==
+                              true) {
+                            Navigator.pushNamed(context, '/profile-edit-pin');
+                          }
+                        },
+                      ),
+                      ProfileMenuItem(
+                        iconUrl: 'assets/ic_wallet.png',
+                        title: 'Wallet Settings',
+                        onTap: () {},
+                      ),
+                      ProfileMenuItem(
+                        iconUrl: 'assets/ic_reward.png',
+                        title: 'My Reward',
+                        onTap: () {},
+                      ),
+                      ProfileMenuItem(
+                        iconUrl: 'assets/ic_help.png',
+                        title: 'Help Center',
+                        onTap: () {},
+                      ),
+                      ProfileMenuItem(
+                        iconUrl: 'assets/ic_logout.png',
+                        title: 'Logout',
+                        onTap: () {
+                          context.read<AuthBloc>().add(AuthLogout());
+                        },
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              const SizedBox(height: 87),
-              CustomTextButton(
-                title: 'Report a Problem',
-                onPressed: () {},
-              ),
-              const SizedBox(height: 50),
-            ],
-          );
+                const SizedBox(height: 87),
+                CustomTextButton(
+                  title: 'Report a Problem',
+                  onPressed: () {},
+                ),
+                const SizedBox(height: 50),
+              ],
+            );
+          }
+
+          return Container();
         },
       ),
     );
